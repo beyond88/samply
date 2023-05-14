5c7b5948f97f30325fe41ef4cbacc96e6da81d1d
beyond88 <muhin.cse.diu@gmail.com>
2023-05-14 15:24:59 +0600

    Fixed plugin action links

--- includes/Admin/PluginMeta.php
+++ includes/Admin/PluginMeta.php
@@ -10,8 +10,7 @@ namespace Samply\Admin;
 class PluginMeta 
 {
 
-    public function __construct()
-    {
+    public function __construct() {
         add_filter( 'plugin_action_links_' . SAMPLY_BASENAME, [ $this, 'plugin_action_links' ] );
         add_filter( 'plugin_row_meta', [ $this, 'plugin_meta_links' ], 10, 2 );
     }
@@ -23,11 +22,10 @@ class PluginMeta
     * @param    array
     * @return   array
     */
-    public function plugin_action_links( $links ) 
-    {
+    public function plugin_action_links( $links ) {
 
         $links[] = '<a href="' . admin_url( 'admin.php?page=samply#general_settings' ) . '">' . __( 'Settings', 'samply' ) . '</a>';
-		$links[] = '<a href="https://ourtechbro.com/docs">' . __( 'Docs', 'samply' ) . '</a>';
+		$links[] = '<a href="https://github.com/beyond88/samply/wiki">' . __( 'Docs', 'samply' ) . '</a>';
         return $links;
 
     }
@@ -39,15 +37,14 @@ class PluginMeta
     * @param    array string
     * @return   array
     */
-    public function plugin_meta_links( $links, $file ) 
-    {
+    public function plugin_meta_links( $links, $file ) {
         
         if ($file !== plugin_basename( SAMPLY_FILE )) {
 			return $links;
 		}
 
-		$support_link = '<a target="_blank" href="https://ourtechbro.com/support" title="' . __('Get help', 'samply') . '">' . __('Support', 'samply') . '</a>';
-		$home_link = '<a target="_blank" href="https://ourtechbro.com" title="' . __('Plugin Homepage', 'samply') . '">' . __('Plugin Homepage', 'samply') . '</a>';
+		$support_link = '<a target="_blank" href="https://github.com/beyond88/samply/issues" title="' . __('Get help', 'samply') . '">' . __('Support', 'samply') . '</a>';
+		$home_link = '<a target="_blank" href="https://github.com/beyond88/samply" title="' . __('Plugin Homepage', 'samply') . '">' . __('Plugin Homepage', 'samply') . '</a>';
 		$rate_link = '<a target="_blank" href="https://wordpress.org/support/plugin/samply/reviews/#new-post" title="' . __('Rate the plugin', 'samply') . '">' . __('Rate the plugin ★★★★★', 'samply') . '</a>';
 
 		$links[] = $support_link;
