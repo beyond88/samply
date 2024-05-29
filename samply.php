9b4e60b69c3f4244345b2c2c2a096083a5ad33d7
beyond88 <muhin.cse.diu@gmail.com>
2024-05-29 12:12:01 +0600

    Tested on WordPress 6.5.3 and WooCommerce 8.9.1

--- samply.php
+++ samply.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Plugin Name: Samply - WooCommerce Product Sample Solution
  * Description: An ultimate plugin to replicate an actual product with custom prices to order as a sample product.
  * Plugin URI: https://github.com/beyond88/samply
  * Author: Mohiuddin Abdul Kader
  * Author URI: https://github.com/beyond88
- * Version: 1.0.10
+ * Version: 1.0.11
  * License: GPL2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       samply
@@ -21,7 +22,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html 
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -30,25 +31,26 @@ require_once __DIR__ . '/vendor/autoload.php';
 /**
  * The main plugin class
  */
-final class Samply {
+final class Samply
+{
 
     /**
      * Plugin version
      *
      * @var string
      */
-    const version = '1.0.10';
+    const version = '1.0.11';
 
     /**
      * Class constructor
      */
-    private function __construct() {
+    private function __construct()
+    {
         $this->define_constants();
 
-        register_activation_hook( __FILE__, [ $this, 'activate' ] );
+        register_activation_hook(__FILE__, [$this, 'activate']);
 
-        add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
-
+        add_action('plugins_loaded', [$this, 'init_plugin']);
     }
 
     /**
@@ -56,10 +58,11 @@ final class Samply {
      *
      * @return \Samply
      */
-    public static function init() {
+    public static function init()
+    {
         static $instance = false;
 
-        if ( ! $instance ) {
+        if (!$instance) {
             $instance = new self();
         }
 
@@ -71,19 +74,19 @@ final class Samply {
      *
      * @return void
      */
-    public function define_constants() {
-        define( 'SAMPLY_VERSION', self::version );
-        define( 'SAMPLY_FILE', __FILE__ );
-        define( 'SAMPLY_PATH', __DIR__ );
-        define( 'SAMPLY_URL', plugins_url( '', SAMPLY_FILE ) );
-        define( 'SAMPLY_ASSETS', SAMPLY_URL . '/assets' );
-        define( 'SAMPLY_BASENAME', plugin_basename( __FILE__ ) );
-        define( 'SAMPLY_PLUGIN_NAME', 'Samply' );
-        define( 'SAMPLY_MIN_WC_VERSION', '3.1' );
-        define( 'SAMPLY_MINIMUM_PHP_VERSION', '5.6.0' );
-        define( 'SAMPLY_MINIMUM_WP_VERSION', '4.4' );
-        define( 'SAMPLY_MINIMUM_WC_VERSION', '3.1' );
-
+    public function define_constants()
+    {
+        define('SAMPLY_VERSION', self::version);
+        define('SAMPLY_FILE', __FILE__);
+        define('SAMPLY_PATH', __DIR__);
+        define('SAMPLY_URL', plugins_url('', SAMPLY_FILE));
+        define('SAMPLY_ASSETS', SAMPLY_URL . '/assets');
+        define('SAMPLY_BASENAME', plugin_basename(__FILE__));
+        define('SAMPLY_PLUGIN_NAME', 'Samply');
+        define('SAMPLY_MIN_WC_VERSION', '3.1');
+        define('SAMPLY_MINIMUM_PHP_VERSION', '5.6.0');
+        define('SAMPLY_MINIMUM_WP_VERSION', '4.4');
+        define('SAMPLY_MINIMUM_WC_VERSION', '3.1');
     }
 
     /**
@@ -91,21 +94,21 @@ final class Samply {
      *
      * @return void
      */
-    public function init_plugin() {
+    public function init_plugin()
+    {
 
         new Samply\Assets();
         new Samply\Samplyi18n();
 
-        if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+        if (defined('DOING_AJAX') && DOING_AJAX) {
             new Samply\Ajax();
         }
 
-        if ( is_admin() ) {
+        if (is_admin()) {
             new Samply\Admin();
         } else {
             new Samply\Frontend();
         }
-
     }
 
     /**
@@ -113,7 +116,8 @@ final class Samply {
      *
      * @return void
      */
-    public function activate() {
+    public function activate()
+    {
         $installer = new Samply\Installer();
         $installer->run();
     }
@@ -122,7 +126,8 @@ final class Samply {
 /**
  * Initializes the main plugin
  */
-function samply() {
+function samply()
+{
     return Samply::init();
 }
 
